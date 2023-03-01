9b42ff308775e522533bfd8531d9192010c55361
shoebilyas123 <shoebilyas123@gmail.com>
2023-03-01 22:26:54 +0530

    disabled font optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  optimizeFonts: false,
   env: {
     EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
     EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
