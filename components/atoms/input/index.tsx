670d733167bc2d268c660603457d5ef1ea8d4f73
anzal1 <cofclan145@gmail.com>
2023-06-25 00:39:29 +0530

    added some animations and updated hero section

--- components/atoms/input/index.tsx
+++ components/atoms/input/index.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import { styles } from '~/constants/contact';
+import React, { FC } from "react";
+import { styles } from "~/constants/contact";
 
 interface IProps extends React.HTMLProps<any> {
   label?: string;
@@ -7,17 +7,31 @@ interface IProps extends React.HTMLProps<any> {
 
 const Input: FC<IProps> = ({ label, className, type, ...props }) => {
   return (
-    <div className={'mb-2 ' + className}>
+    <div
+      className={"mb-2 " + className}
+      style={{
+        width: "100%",
+        //create curvy border
+
+      }}
+    >
       <label>{label}</label>
       <div
-        className="rounded-lg border border-red-400"
-        style={{ borderWidth: 6, padding: '2px' }}
+        className="rounded-lg border border-red-400 "
+        style={{
+          borderWidth: 4,
+          padding: "2px",
+          //create curvy border 
+          // create shadow
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+
+        }}
       >
-        {type === 'textarea' ? (
+        {type === "textarea" ? (
           <textarea {...props} className={styles.inputClass} rows={10} />
         ) : (
           <input
-            {...{ type: type || 'default', ...props }}
+            {...{ type: type || "default", ...props }}
             className={styles.inputClass}
           />
         )}
